ace63e0deee6b46e15169bada42003fa0ace4d2d
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-07-06 18:01:31 +1000

    fixing unicode problems and adding dom editing

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 function dropdownClick(x) {
-    document.getElementById("myDropdown").classList.toggle("show");
-    x.classList.toggle("change")
+  document.getElementById("myDropdown").classList.toggle("show");
+  x.classList.toggle("change");
 }
+document.addEventListener("DOMContentLoaded", () => {
+  let nodeList = document.querySelectorAll("span.tlable");
+  for (let i = 0; i < nodeList.length; i++) {
+    console.log(nodeList[i]);
+  }
+});
