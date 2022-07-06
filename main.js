f2da75b665573d7ce4e387923da5e7945ded7399
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-07-06 19:13:08 +1000

    Hover Translate working but buggy

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ function dropdownClick(x) {
   document.getElementById("myDropdown").classList.toggle("show");
   x.classList.toggle("change");
 }
+
 document.addEventListener("DOMContentLoaded", () => {
-  let nodeList = document.querySelectorAll("span.tlable");
-  for (let i = 0; i < nodeList.length; i++) {
-    console.log(nodeList[i]);
+  let tlables = document.querySelectorAll("span.tlable");
+  let tlations = document.querySelectorAll("span.tlation");
+  for (let i = 0; i < tlables.length; i++) {
+    tlables[i].addEventListener("mouseenter", () => {
+        tlations[i].style.display = "inherit";
+    });
+    tlables[i].addEventListener("mouseleave", () => {
+        tlations[i].style.display = "none";
+    });
   }
 });
